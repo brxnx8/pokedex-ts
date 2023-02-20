57b7304b845dce6b7a76307c654a162ff9708f30
Bruno oliveira <brunogabriel2608@gmail.com>
2023-02-19 23:37:54 -0300

    adicionando imagem de pokebola para capturar o pokemon

--- src/assets/components/cardinformations/index.tsx
+++ src/assets/components/cardinformations/index.tsx
@@ -1,7 +1,12 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useEffect, useState } from "react";
 import { Pokemon } from "../../../App";
 import { DivColored, Types } from "../divColors/div.styles";
 import { Stats } from "../stats";
+
+import PokeballStopped from "../../images/pokeballStopped.gif";
+import PokeballRotated from "../../images/pokeballRotated.gif";
+import PokeballStars from "../../images/pokeballStars.png";
+
 import style from "./cardinformations.module.css";
 
 
@@ -13,21 +18,35 @@ interface PropsCardInfo{
 
 export function CardInfo({ pokemon, classCard, ToggleClassCard }: PropsCardInfo) {
     
+    const [Pokeball, setPokeball] = useState(PokeballStopped);
+    
+    useEffect(() => setPokeball(PokeballStopped), [pokemon]);
+
     let types = new Array();
 
     for (let obj in pokemon.types) {
         types.push(pokemon.types[parseInt(obj)].type.name);
     }
 
+    function TogglePokeball() {
+        if(Pokeball != PokeballStars){
+            setPokeball(PokeballRotated);
+            setTimeout(() => setPokeball(PokeballStars), 1200);
+        }
+
+    }
+
     return (
         
         <section className={classCard} onClick={ToggleClassCard}>
             <div className={style.card}>
+                
                 <DivColored 
                     color={types[0]} 
                     className={style.divColorCard}
                 >
-                    <img src={pokemon.imageCard} alt="" />
+                    <img src={Pokeball} alt="" width={"40px"} height={"40px"} onClick={TogglePokeball} className={style.pokeball}/>
+                    <img src={pokemon.imageCard} alt="" className={style.pokeImage}/>
                 </DivColored>
                 <div className={style.pokemonInfo}>
                     <h1>{pokemon.name}</h1>
